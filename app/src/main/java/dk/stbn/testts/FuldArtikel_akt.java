cc5790364d922b0c52cc6cb7e224edb187ee86c7
sune <sunetb@gmail.com>
2017-05-14 22:54:59 +0200

    Pil som angiver at der kan scrolles virker i begge visninger. Men skal fixes: vises igen ved tryk på et element i listen (kald til opdaterUI()) Langgt klik på element i listen ELLER tryk på knappen "MERE" fører til ny aktivitet

---
+++ app/src/main/java/dk/stbn/testts/FuldArtikel_akt.java
@@ -0,0 +1,16 @@
+package dk.stbn.testts;
+import android.support.v7.app.*;
+import android.os.*;
+
+public class FuldArtikel_akt extends AppCompatActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		// TODO: Implement this method
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.fuldartikel);
+	}
+	
+}
